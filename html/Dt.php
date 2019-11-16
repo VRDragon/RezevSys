382b41227a6a8603ad031de2b354dacadd9837ac
VRDragon <30727840+VRDragon@users.noreply.github.com>
2019-11-16 12:28:45 +0100

    Update Dt.php

--- html/Dt.php
+++ html/Dt.php
@@ -10,7 +10,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$sql = "UPDATE Zakaznici SET klient = 'Odber/Termin',Priorita = '2'  WHERE id='1'";
+$sql = "UPDATE Zakaznici SET klient = 'Odber/Termín',Priorita = '2'  WHERE id='1'";
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
