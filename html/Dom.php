1b529339ef841f4dded63de7416e36b131f3d123
VRDragon <30727840+VRDragon@users.noreply.github.com>
2019-11-16 09:59:38 +0100

    Create Dom.php

---
+++ html/Dom.php
@@ -0,0 +1,31 @@
+<?php
+$sec = "7";
+echo ($page);
+header("Refresh: $sec; url=index.php");
+?>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Reletronic Rezerva ^mny system</title>
+<style>
+h1
+{
+font-family: georgia;
+font-weight: bold;
+background-color:#fff;
+width:100%;
+color:#000;
+padding:10px;
+font-size:70px;
+margin-bottom:1px;
+margin-top:1px;
+position: relative;
+top: 50%;
+transform: translateY(-50%);
+}
+</style>
+</head>
+   <body style="text-align:center;">
+        <h1 style="color:#000;">ďakujeme Vám za záujem o naše poradenstvo. Z dôvodu vyťaženosti Vám prosíme, aby ste si dohodli termín cez email rezo@forled.sk alebo telefonicky na 0917 972 683. Prosím rezervujte si na to 2 hodiny času.<br />
+		 </body>
+</html>
