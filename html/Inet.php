b714166602a3309703b528adeae94cd7b2d9629a
VRDragon <30727840+VRDragon@users.noreply.github.com>
2019-11-16 10:05:34 +0100

    Create Inet.php

---
+++ html/Inet.php
@@ -0,0 +1,31 @@
+<?php
+$sec = "7";
+echo ($page);
+header("Refresh: $sec; url=index.php");
+?>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Reletronic Rezerva ^mny system</title>
+<style>
+h1
+{
+font-family: georgia;
+font-weight: bold;
+background-color:#fff;
+width:100%;
+color:#000;
+padding:10px;
+font-size:70px;
+margin-bottom:1px;
+margin-top:1px;
+position: relative;
+top: 50%;
+transform: translateY(-50%);
+}
+</style>
+</head>
+   <body style="text-align:center;">
+        <h1 style="color:#000;">Poprosím o chvíľu strpenia, o chvíľu príde kolegynka a dohodne s Vami čo potrebujete.<br />
+		 </body>
+</html>
