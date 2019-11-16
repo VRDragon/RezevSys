347363f9a87d07b0ea87bf49aa394d319dc59927
VRDragon <30727840+VRDragon@users.noreply.github.com>
2019-11-16 10:03:33 +0100

    Create Naku.php

---
+++ html/Naku.php
@@ -0,0 +1,32 @@
+<?php
+$sec = "7";
+echo ($page);
+header("Refresh: $sec; url=index.php");
+?>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Reletronic Rezerva ^mny system</title>
+<style>
+h1
+{
+font-family: georgia;
+font-weight: bold;
+background-color:#fff;
+width:100%;
+color:#000;
+padding:10px;
+font-size:70px;
+margin-bottom:1px;
+margin-top:1px;
+position: relative;
+top: 50%;
+transform: translateY(-50%);
+}
+</style>
+</head>
+   <body style="text-align:center;">
+        <h1 style="color:#000;">Ďakujeme Vám za Váš záujem o naše produkty. Bohužiaľ nie sme klasická predajňa, sme výdajňa tovaru k eshopu, preto Vás nevieme vybaviť na počkanie <br />
+        <h1 style="color:#000;">Urobte si prosím objednávku na eshope www.forled.sk a hneď ako bude objednávka pripravená k ososbnému odberu budeme Vás kontaktovať.<br />
+		 </body>
+</html>
