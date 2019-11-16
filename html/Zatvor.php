4c2e2e337f9a5560ab735bb0ecd03fd0f409a017
VRDragon <30727840+VRDragon@users.noreply.github.com>
2019-11-16 11:41:20 +0100

    Create Zatvor.php

---
+++ html/Zatvor.php
@@ -0,0 +1,19 @@
+<?php
+$servername = "localhost";
+$username = "admin";
+$password = "test";
+$dbname = "RezSys";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$sql = "UPDATE oznamTab SET txt = 'Zatvorené',Status = '1'  WHERE id='1'";
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
+?>
