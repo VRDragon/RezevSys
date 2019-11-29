28915393e12e0f2ba612fd1b5bd326ffd87c33ad
VRDragon <30727840+VRDragon@users.noreply.github.com>
2019-11-29 09:16:32 +0100

    Update Otvor.php

--- html/Otvor.php
+++ html/Otvor.php
@@ -9,7 +9,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$sql = "UPDATE oznamTab SET txt = 'Otvorené',Status = '1'  WHERE id='1'";
+$sql = "UPDATE oznamTab SET txt = 'Otvorené 8:00-15:00',Status = '1'  WHERE id='1'";
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
