f5857086a92577673e307520b864b381b641c275
VRDragon <30727840+VRDragon@users.noreply.github.com>
2019-12-19 12:29:33 +0100

    Update Ch.php

--- html/Ch.php
+++ html/Ch.php
@@ -10,8 +10,8 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$text= 'chcem nakupit'.date('H:i');
-$sql = "UPDATE Zakaznici SET klient = $text,Priorita = '6'  WHERE id='1'";
+$text= 'chcem nakupit '.date('H:i');
+$sql = "UPDATE Zakaznici SET klient = ".$text.",Priorita = '6'  WHERE id='1'";
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
