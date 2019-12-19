9b9716dffe32c832a31adfcb8132a57122e9cb1a
VRDragon <30727840+VRDragon@users.noreply.github.com>
2019-12-19 12:38:55 +0100

    Update Vo.php

--- html/Vo.php
+++ html/Vo.php
@@ -10,7 +10,8 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$sql = "UPDATE Zakaznici SET klient = 'Veľkoobchod',Priorita = '1' WHERE id='1'";
+$text= 'Veľkoobchod '.date('H:i');
+$sql = "UPDATE Zakaznici SET klient ='$text',Priorita = '1'  WHERE id='1'";
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
