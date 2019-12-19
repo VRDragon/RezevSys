77ee51b4b88f6a0c543c0f8eb02dc24c3ce898da
VRDragon <30727840+VRDragon@users.noreply.github.com>
2019-12-19 12:38:04 +0100

    Update Po.php

--- html/Po.php
+++ html/Po.php
@@ -10,7 +10,8 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$sql = "UPDATE Zakaznici SET klient = 'projekt',Priorita = '3'  WHERE id='1'";
+$text= 'Projekt'.date('H:i');
+$sql = "UPDATE Zakaznici SET klient ='$text',Priorita = '3'  WHERE id='1'";
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
