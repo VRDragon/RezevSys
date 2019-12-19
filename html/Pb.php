ead55bca94af9d05f5e668fdf64c6aecfad3526e
VRDragon <30727840+VRDragon@users.noreply.github.com>
2019-12-19 12:37:13 +0100

    Update Pb.php

--- html/Pb.php
+++ html/Pb.php
@@ -10,7 +10,8 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$sql = "UPDATE Zakaznici SET klient = 'Byt/Dom',Priorita = '5'  WHERE id='1'";
+$text= 'Byt/Dom'.date('H:i');
+$sql = "UPDATE Zakaznici SET klient ='$text',Priorita = '5'  WHERE id='1'";
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
